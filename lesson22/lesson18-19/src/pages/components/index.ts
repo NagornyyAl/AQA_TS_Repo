a315bcb0cdb63af7c577ed30c65b249020497655
QAlexN <OleksandrN@terra.lan>
2026-07-20 00:15:13 +0300

    Added files lesson22

---
+++ lesson22/lesson18-19/src/pages/components/index.ts
@@ -0,0 +1,4 @@
+export { CatalogMenuComponent } from './catalog-menu.component';
+export { SearchBoxComponent } from './search-box.component';
+export { BreadcrumbsComponent } from './breadcrumbs.component';
+export { ProductListComponent } from './product-list.component';
